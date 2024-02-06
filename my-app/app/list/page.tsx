1b98a440a0dc61c12fb4d6811571be08b0306fe7
youngeunkim <pkhj286@gmail.com>
2024-02-06 18:33:07 +0900

    Add : 이미지 추가

--- my-app/app/list/page.tsx
+++ my-app/app/list/page.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
 
-export default function list() {
+export default function List() {
   const 상품 = ["Tomatoes", "Pasta", "Chicken"];
+
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      {상품.map((index) => {
-        return (
-          <div className="food">
-            <h4>{index} $40</h4>
-          </div>
-        );
-      })}
+      {상품.map((productName, index) => (
+        <div className="food" key={index}>
+          <h4>{productName} $40</h4>
+          <Image src="/food1.jpg" alt="음식" width={200} height={200} />
+        </div>
+      ))}
     </div>
   );
 }
