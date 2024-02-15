85943812392e736291dfe8e1fe8285cf66e71222
youngeunkim <pkhj286@gmail.com>
2024-02-15 10:03:07 +0900

    map으로 사진 돌리기

--- my-app/app/list/page.tsx
+++ my-app/app/list/page.tsx
@@ -1,15 +1,24 @@
 import Image from "next/image";
 
 export default function List() {
-  const 상품 = ["Tomatoes", "Pasta", "Chicken"];
+  const 상품 = [
+    { name: "Tomatoes", image: "food1.jpg" },
+    { name: "Pasta", image: "food2.jpg" },
+    { name: "Chicken", image: "food3.jpg" },
+  ];
 
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      {상품.map((productName, index) => (
+      {상품.map((product, index) => (
         <div className="food" key={index}>
-          <h4>{productName} $40</h4>
-          <Image src="/food1.jpg" alt="음식" width={200} height={200} />
+          <h4>{product.name} $40</h4>
+          <Image
+            src={`/${product.image}`}
+            alt={product.name}
+            width={200}
+            height={200}
+          />
         </div>
       ))}
     </div>
