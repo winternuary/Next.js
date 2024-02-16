c881ab7e1fd50bd0a9e70fa5b1b01f302d944fd8
youngeunkim <pkhj286@gmail.com>
2024-02-16 10:33:04 +0900

    Add : 상품 장바구니

--- my-app/app/cart/page.tsx
+++ my-app/app/cart/page.tsx
@@ -1,8 +1,19 @@
 import Link from "next/link";
+
 export default function cart() {
   return (
     <div>
-      <h2 className="cart">장바구니 입니다</h2>
+      <h4 className="title">Cart</h4>
+      <div className="cart-item">
+        <p>상품명</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
+      <div className="cart-item">
+        <p>상품명</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
       <Link href="/cart/payment">
         <button className="cartBtn">결제 하기</button>
       </Link>
