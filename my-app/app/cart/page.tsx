d7c91d57708e50798cd2a237a5b8be395fa1254a
youngeunkim <pkhj286@gmail.com>
2024-02-16 10:37:29 +0900

    refactor: 컴포넌트 분리

--- my-app/app/cart/page.tsx
+++ my-app/app/cart/page.tsx
@@ -4,20 +4,24 @@ export default function cart() {
   return (
     <div>
       <h4 className="title">Cart</h4>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <Cartlist />
+      <Cartlist />
       <Link href="/cart/payment">
         <button className="cartBtn">결제 하기</button>
       </Link>
       <p>현대카드 결제시 무이자 이벤트</p>
+    </div>
+  );
+}
+
+function Cartlist() {
+  return (
+    <div>
+      <div className="cart-item">
+        <p>상품명</p>
+        <p>$40</p>
+        <p>1개</p>
+      </div>
     </div>
   );
 }
