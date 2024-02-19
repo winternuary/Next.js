4be84854243a0e49631000d4fa1340f60ec7c731
youngeunkim <pkhj286@gmail.com>
2024-02-19 13:30:20 +0900

    props 응용

--- my-app/app/cart/page.tsx
+++ my-app/app/cart/page.tsx
@@ -4,10 +4,14 @@ export default function cart() {
     <div>
       <h4 className="title">Cart</h4>
       <Cartlist item={장바구니[0]} />
-      <Cartlist />
-      <Cartlist />
+      <Cartlist item={장바구니[1]} />
+      <Banner content="롯데카드" />
     </div>
   );
+}
+
+function Banner(props: { content?: string }) {
+  return <h5>{props.content} 결제 행사 중</h5>;
 }
 
 function Cartlist(props: { item?: string }) {
