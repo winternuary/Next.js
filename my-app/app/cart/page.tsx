c930a4bffcdf7b27ad93722f2b8a108a6fbe9118
youngeunkim <pkhj286@gmail.com>
2024-02-20 22:50:00 +0900

    props 색 바꾸기

--- my-app/app/cart/page.tsx
+++ my-app/app/cart/page.tsx
@@ -5,13 +5,13 @@ export default function cart() {
       <h4 className="title">Cart</h4>
       <Cartlist item={장바구니[0]} />
       <Cartlist item={장바구니[1]} />
-      <Banner content="롯데카드" />
+      <Banner content="롯데카드" color="red" />
     </div>
   );
 }
 
-function Banner(props: { content?: string }) {
-  return <h5>{props.content} 결제 행사 중</h5>;
+function Banner(props: { content?: string; color: string }) {
+  return <h5 style={{ color: props.color }}>{props.content} 결제 행사 중</h5>;
 }
 
 function Cartlist(props: { item?: string }) {
