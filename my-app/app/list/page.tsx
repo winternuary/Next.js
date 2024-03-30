839239321862d72bb633970e5765b05845f2ce91
youngeunkim <pkhj286@gmail.com>
2024-03-30 23:23:35 +0900

    배열복사

--- my-app/app/list/page.tsx
+++ my-app/app/list/page.tsx
@@ -9,11 +9,43 @@ export default function List() {
     { name: "Pasta", image: "food2.jpg" },
     { name: "Chicken", image: "food3.jpg" },
   ];
-  let [수량, 수량변경] = useState(0);
+  let [수량, 수량변경] = useState([0, 0, 0]);
 
   return (
     <div>
       <h4 className="title">상품목록</h4>
+
+      <span>{수량[0]}</span>
+      <button
+        onClick={() => {
+          let copy = [...수량]; // 독립적인 array 만들어서 복사해줌
+          copy[0]++;
+          수량변경(copy);
+        }}
+      >
+        +
+      </button>
+      <span>{수량[1]}</span>
+      <button
+        onClick={() => {
+          let copy = [...수량]; // 독립적인 array 만들어서 복사해줌
+          copy[1]++;
+          수량변경(copy);
+        }}
+      >
+        +
+      </button>
+      <span>{수량[2]}</span>
+      <button
+        onClick={() => {
+          let copy = [...수량]; // 독립적인 array 만들어서 복사해줌
+          copy[2]++;
+          수량변경(copy);
+        }}
+      >
+        +
+      </button>
+
       {상품.map((product, index) => (
         <div className="food" key={index}>
           <h4>{product.name} $40</h4>
@@ -24,20 +56,6 @@ export default function List() {
             height={200}
           />
           <span>{수량}</span>
-          <button
-            onClick={() => {
-              수량변경(수량 + 1);
-            }}
-          >
-            +
-          </button>
-          <button
-            onClick={() => {
-              수량변경(수량 - 1);
-            }}
-          >
-            -
-          </button>
         </div>
       ))}
     </div>
